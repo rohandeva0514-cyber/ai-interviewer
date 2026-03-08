dc6052f7441b6acfc5215be561468e4eb0564467
Rohan Devadiga <rohandeva0514@gmail.com>
2026-03-08 15:56:29 +0530

    restrict cors to frontend domain

--- Backend/run.py
+++ Backend/run.py
@@ -15,7 +15,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ai-interviewer-tau-nine.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
